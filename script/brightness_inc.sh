a06bc45ffde79f0f27900393f3567c0295529bed
mscheuren <m.scheuren@oyra.eu>
2017-02-08 15:33:56 +0100

    brighness inc/dec faster in the middel

--- script/brightness_inc.sh
+++ script/brightness_inc.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 current_brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
-echo $((current_brightness+10)) | tee /sys/class/backlight/intel_backlight/brightness
+if [ "$current_brightness" -ge 250 ] && [ "$current_brightness" -le 500 ]; then
+	echo $((current_brightness+20)) | tee /sys/class/backlight/intel_backlight/brightness
+else
+        echo $((current_brightness+10)) | tee /sys/class/backlight/intel_backlight/brightness
+
+fi
 
