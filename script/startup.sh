1a269ac9ff41b10ee257a592a47680cd478d41d6
Moritz Scheuren <m.scheuren@oyra.eu>
2017-03-03 18:35:27 +0100

    start seafile

--- script/startup.sh
+++ script/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh 
 sleep 1s 
 trayer --edge top --align center --SetDockType true --tint 0x1c1c1c --SetPartialStrut false --expand true --height 18 --widthtype pixel --width 200
+seafile-applet
 exit 0
