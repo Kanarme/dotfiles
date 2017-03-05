6e7f372fc6fffcdb2862618da754b1c35d0db416
mscheuren <m.scheuren@oyra.eu>
2017-03-05 16:47:22 +0100

    cleanup

--- nixos/p50/config.nix
+++ nixos/p50/config.nix
@@ -1,57 +1,56 @@
 {
-  system.autoUpgrade.channel = "https://nixos.org/channel/nixos-unstable";
-  allowUnfree = true;
-  packageOverrides = pkgs_: with pkgs_; {
-    all = with pkgs; buildEnv {
-      name = "all";
-      paths = [
-        chromium
-	      cmake
-	      gajim
-	      hakuneko
-        mirage
-	      mumble
-	perlPackages.ImageExifTool
-#	python27Packages.nbxmpp
-#	python27Packages.python-axolotl
-#	python27Packages.cryptography
-#	python27Packages.protobuf2_6
-	sc-im
-	speedcrunch	
-	qutebrowser
-	qemu
-	steam
-	virtmanager
-	vlc
-	xarchiver
-	xfce.thunar
-  ledger
-  mylatex
-  myemacs
-      ];
-    };
+system.autoUpgrade.channel = "https://nixos.org/channel/nixos-unstable";
+allowUnfree = true;
+    packageOverrides = pkgs_: with pkgs_; {
+                 all = with pkgs; buildEnv {
+                 name = "all";
+                 paths = [
+                       chromium
+	                     cmake
+	                     gajim
+	                     hakuneko
+                       mirage
+	                     mumble
+                       perlPackages.ImageExifTool
+                       # python27Packages.nbxmpp
+                       # python27Packages.python-axolotl
+                       # python27Packages.cryptography
+                       # python27Packages.protobuf2_6
+                       sc-im
+                       speedcrunch	
+                       qutebrowser
+                       qemu
+                       steam
+                       virtmanager
+                       vlc
+                       xarchiver
+                       xfce.thunar
+                       mylatex
+                       myemacs
+                 ];
+        };
     
-  myemacs = with pkgs; buildEnv {
-      name = "myemacs";
-      paths = [
-	          auctex
-	          emacs
-	          gv
-		        pandoc
-	          source-code-pro
-	          xclip
-	          ];
-    };
-  mylatex = with pkgs; buildEnv {
-      name = "mylatex";
-      paths = [
-            biber
-            jabref
-            jdk
-           	texlive.combined.scheme-full
-            texmaker
-	          ];
-    };
+        myemacs = with pkgs; buildEnv {
+              name = "myemacs";
+              paths = [
+                    auctex
+                    emacs
+                    gv
+                    pandoc
+                    source-code-pro
+                    xclip
+              ];
+        };
 
-  };
+        mylatex = with pkgs; buildEnv {
+             name = "mylatex";
+             paths = [
+                biber
+                # jabref
+                # jdk
+                texlive.combined.scheme-full
+                texmaker
+              ];
+        };
+    };
 }
