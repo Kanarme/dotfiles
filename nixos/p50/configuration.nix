dd19a9db941fe4be174ec52dd45fef0e36da66e2
Nixuse <nixuse@oyra.eu>
2017-04-21 14:27:59 +0200

    switch to bspwm and Cleanup

--- nixos/p50/configuration.nix
+++ nixos/p50/configuration.nix
@@ -22,11 +22,13 @@ environment.systemPackages = with pkgs; [
 	rofi
 	trayer
 	xorg.xrandr
-	
-	# Software
+
+  # Software
 	beancount
 	calibre
 	colordiff
+	dwarf-fortress
+	dwarf-fortress-packages.phoebus-theme
 	fava
 	gparted
 	htop
@@ -44,13 +46,14 @@ environment.systemPackages = with pkgs; [
 	seafile-client
 	shutter
 	ssvnc
+	steam
 	taskwarrior
 	thunderbird
 	tig
 	tldr
 	x11vnc
 	zathura  # PDF Viewer
-	
+
 	# System
 	arandr
 	bashmount
@@ -59,7 +62,7 @@ environment.systemPackages = with pkgs; [
 	davfs2
 	exfat
 	fam
-	ghc     
+	ghc
 	git
 	gxmessage
 	jdk
@@ -69,8 +72,8 @@ environment.systemPackages = with pkgs; [
 	networkmanagerapplet
 	ntfs3g
 	numlockx
-	pasystray	
-	pciutils     
+	pasystray
+	pciutils
 	python
 	python27Packages.pip
 	python27Packages.setuptools
@@ -83,37 +86,43 @@ environment.systemPackages = with pkgs; [
 	xdotool
 	xorg.libXt
 	xorg.libXtst
-	
-	# Writing
+
+  # Writing
 	aspell
 	aspellDicts.de
 	aspellDicts.en
 	# texlive.combined.scheme-full
 ];
 
-      
+
  ####################################################################################################################################################################
  # Services
 
  services.xserver = {
          # Graphic
-	 # videoDrivers = [ "nouveau" ];
-         
+	 videoDrivers = [ "intel nvidia" ];
+
 	 # keyboard
          enable = true;
          layout = "us";
 	 xkbOptions = "eurosign:e";
-  	 
-         # Notebook
+
+   # Notebook
 	 synaptics.enable = true;
 	 synaptics.twoFingerScroll = true;
 
-         # UI         
-	 windowManager.xmonad.enable = true;
-         windowManager.default = "xmonad";
-         windowManager.xmonad.enableContribAndExtras = true;
+   # UI
+	 #windowManager.xmonad.enable = true;
+         #windowManager.default = "xmonad";
+         #windowManager.xmonad.enableContribAndExtras = true;
+         #desktopManager.xterm.enable = false;
+	 windowManager.bspwm.enable = true;
+         windowManager.default = "bspwm";
+         windowManager.bspwm.configFile = "/home/user/.dotfiles/bspwm/bspwmrc";
+         windowManager.bspwm.sxhkd.configFile= "/home/user/.dotfiles/bspwm/sxhkdrc";
          desktopManager.xterm.enable = false;
-	 displayManager.auto = {
+
+   displayManager.auto = {
 	 	enable = true;
 		user = "user";
 	 };
@@ -126,23 +135,23 @@ environment.systemPackages = with pkgs; [
 services.gnome3.gvfs.enable = true;
 services.openssh.forwardX11 = true;
  #services.teamviewer.enable  = true;
- 
+
  ####################################################################################################################################################################
  # Gerneral config
-  
+
  	 # Kernel
 	 # boot.kernelPackages = pkgs.linuxPackages_latest; #_4_9; #_latest;
-	 
-	 # System Language
+
+   	# System Language
 	 i18n = {
         	defaultLocale = "en_US.UTF-8";
 	 };
 
 	 # Time
   	 time.timeZone = "Europe/Berlin";
-  	 
+
 	 # Mount
-	 # fileSystems."/oyra" = 
+	 # fileSystems."/oyra" =
 	 # {
 	 # device = "https://cloud.test.eu/seafdav";
    	 # options = ["noauto,users,rw"];
@@ -153,6 +162,27 @@ services.openssh.forwardX11 = true;
   	 user ALL=(ALL) NOPASSWD: /home/user/.dotfiles/script/webdav.sh
 	 '';
 
+
+	 # Fonts
+	 fonts = {
+		enableFontDir = true;
+		enableGhostscriptFonts = true;
+	 	fontconfig.ultimate.enable = true;
+		fonts = with pkgs; [
+	     	siji		
+		#	nerdfonts
+	     	# 	inconsolata
+	     	# 	terminus_font
+	     	# 	proggyfonts
+	     	#	dejavu_fonts
+	     	# 	font-awesome-ttf
+	     	# 	ubuntu_font_family
+	     	# 	source-code-pro
+	     	# 	source-sans-pro
+	     	# 	source-serif-pro
+	    	];
+	  };
+
 	 # Redshift
 	 services.redshift = {
 	     enable = true;
@@ -162,12 +192,13 @@ services.openssh.forwardX11 = true;
 
 	 # Network
    	 networking.networkmanager.enable = true;
-   	 
+
 	 # Hardware
 	 hardware = {
 	     bumblebee = {
 	     	enable = true;
 	        connectDisplay = true;
+	     	driver = "nvidia";
 	     };
 
 	     pulseaudio = {
@@ -175,33 +206,32 @@ services.openssh.forwardX11 = true;
 		support32Bit = true;
 	     };
 
-	     opengl.driSupport32Bit = true; 
+	     opengl.driSupport32Bit = true;
 	 };
-	 
+
 	 # Printer
   	 services.printing = {
    	     enable = true;
 	     drivers = [ pkgs.samsungUnifiedLinuxDriver ];
   	 };
-         
+
          # ZSH
          programs.zsh = {
 	 	enable = true;
 		interactiveShellInit = ''
       			export EDITOR=nvim
-      			
 			# Java
       		 	export _JAVA_AWT_WM_NONREPARENTING=1
       		'';
-		
+
 		shellAliases = {
-			ls="ls --color=auto";
-			l="ls -alh";
-      			grep="grep -i --color=auto";
-    		};
-	 };
+				ls="ls --color=auto";
+				l="ls -alh";
+	    			grep="grep -i --color=auto";
+	    		};
+		 };
          users.defaultUserShell = "/run/current-system/sw/bin/zsh";
-	 
+	
 	 # light
 	 programs.light.enable = true;
 
@@ -210,49 +240,48 @@ services.openssh.forwardX11 = true;
 
 	 # SWAP
 	 swapDevices = [{
-	 device = "/var/cache/swap/swap0";
+		 device = "/var/cache/swap/swap0";
 	 }];
 	 #swapDevices.*.size = "1024";
-
+	
 	 # Clean up
 	 nix.gc.automatic = true;
 	 nix.gc.dates = "16:14";
 	 nix.gc.options = "--delete-older-than 30d";
-	 nix.extraOptions = 
-	 ''
+	 nix.extraOptions = ''
 	 gc-keep-output = true
 	 gc-keep-derivations = true
 	 auto-optimise-stor = true
 	 '';
-
+	
 	 # boot.cleanTmpDir = true;
-
+	
 	 # Search
 	 services.locate.enable = true;
 	 services.locate.interval = "10 * * * *";
 
  ####################################################################################################################################################################
  # Users
- 
+
  # user
  users.extraUsers.user = {
          isNormalUser = true;
          home = "/home/user";
          extraGroups = ["davfs2""wheel" "networkmanager" "vboxusers"];
  };
- 
+
  ####################################################################################################################################################################
  # Channel
- # system.autoUpgrade.channel = "https://nixos.org/channels/nixos-unstable"; 
+ # system.autoUpgrade.channel = "https://nixos.org/channels/nixos-unstable";
  # Channel List https://nixos.org/channels/
  # Config Version
  system.stateVersion = "17.03";
- 
+
  ####################################################################################################################################################################
  # Bootloader
  boot.loader.systemd-boot.enable = true;
  boot.loader.efi.canTouchEfiVariables = true;
- 
+
 ####################################################################################################################################################################
  # Virtualisation
  #virtualisation.libvirtd.enable = true;
